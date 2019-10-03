ebf0aca22b78287637b8a22eb2a6c01c1f335f45
meryem00 <meryem.a.sherief@gmail.com>
2019-10-03 13:10:44 +0300

    Added room stuff day1

---
+++ app/src/main/java/com/gebeya/fetan/data/model/Run.java
@@ -0,0 +1,18 @@
+package com.gebeya.fetan.data.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+
+@Entity(tableName = "runs")
+public class Run {
+    @ColumnInfo
+    public double distance;
+
+    @ColumnInfo
+    public int time;
+
+    @ColumnInfo(name = "created_at")
+    public String createdAt;
+
+
+}
